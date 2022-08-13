3eeb45107818c437d436d58d8557e3451064aedc
subv9521 <buisu.humg@gmail.com>
2022-08-13 14:24:27 +0700

    shop clothes 13/8

---
+++ script.js
@@ -0,0 +1,8 @@
+let getShowMenu = document.querySelector('.menu-mobile');
+function showMenu() {
+    getShowMenu.classList.add('d-block');
+}
+
+function hideShowMenu() {
+    getShowMenu.classList.remove('d-block')
+}

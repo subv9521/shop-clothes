007a623d3f30c3f75c03207d9c91d5d359962c4f
subv9521 <buisu.humg@gmail.com>
2022-09-10 21:46:37 +0700

    update 10/9

--- script.js
+++ script.js
@@ -6,3 +6,55 @@ function showMenu() {
 function hideShowMenu() {
     getShowMenu.classList.remove('d-block')
 }
+
+// slide //
+const getImgContainer = document.querySelector('.img-slide')
+const getImgSlide = document.querySelectorAll('.img-slide-link')
+
+const getSlideAction = document.querySelector('.slide-action')
+const getArrowLeft = document.querySelector('.slide-arrow-left')
+const getArrowRight = document.querySelector('.slide-arrow-right')
+
+let slideAction = document.querySelector('.slide-action p')
+let indexImg = 1;
+let imgNumber = getImgSlide.length;
+
+getImgSlide.forEach((img, index) => {
+    img.style.left = index*100 + '%';
+})
+
+function slideShow(number) {
+    getImgContainer.style.left = `${-number*100}%`;
+}
+
+getArrowLeft.addEventListener('click', () => {
+    indexImg--
+    if (indexImg < 1) {
+        indexImg = imgNumber;
+    }
+    slideShow(indexImg - 1);
+    slideAction.innerHTML = `${indexImg}/${imgNumber}`;
+})
+
+getArrowRight.addEventListener('click', () => {
+    indexImg++
+    if (indexImg > imgNumber) {
+        indexImg = 1;
+    }
+    slideShow(indexImg - 1);
+    slideAction.innerHTML = `${indexImg}/${imgNumber}`;
+})
+
+function autoSlide() {
+    indexImg++
+    if (indexImg > imgNumber) {
+        indexImg = 1;
+    }
+    slideShow(indexImg - 1);
+    slideAction.innerHTML = `${indexImg}/${imgNumber}`;
+}
+
+setInterval(autoSlide, 5000);
+
+
+
